f49adbae1e6d831e02c1a4ed6e64ff2295ab9294
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-03-01 00:13:04 +0000

    Less redundant type definitions for Workspace

--- web/src/workspace.tsx
+++ web/src/workspace.tsx
@@ -4,40 +4,30 @@ import Expr from "./expr";
 import * as E from "./expr";
 import * as Samples from "./sample";
 
+type TopLevel = { [name: string]: Expr };
+
+export type WorkspaceValue = WorkspaceProvider["state"];
+
 export const Workspace = React.createContext<Optional<WorkspaceValue>>(null);
-
-export interface WorkspaceValue {
-    topLevel: { [name: string]: Expr };
-    getTopLevel(name: string): Expr;
-    removeTopLevel(name: string): void;
-    setTopLevel(name: string, update: (expr: Expr) => Expr): void;
-}
-
-export class WorkspaceProvider extends Component<{}, WorkspaceValue> {
-    state: WorkspaceValue = {
-        topLevel: { "Sample 1": Samples.SAMPLE_1, "Sample 2": Samples.SAMPLE_2 },
-        removeTopLevel: this.removeTopLevel.bind(this),
-        setTopLevel: this.setTopLevel.bind(this),
-        getTopLevel: this.getTopLevel.bind(this),
+export class WorkspaceProvider extends Component<{}, WorkspaceProvider["state"]> {
+    state = {
+        topLevel: { "Sample 1": Samples.SAMPLE_1, "Sample 2": Samples.SAMPLE_2 } as TopLevel,
+        removeTopLevel: (name: string) => {
+            this.setState(({ topLevel }) => {
+                const cloned = Object.assign({}, topLevel);
+                delete cloned[name];
+                return { topLevel: cloned };
+            });
+        },
+        setTopLevel: (name: string, update: (expr: Expr) => Expr) => {
+            this.setState(({ topLevel }) => ({
+                topLevel: { ...topLevel, [name]: update(topLevel[name]) },
+            }));
+        },
+        getTopLevel: (name: string): Expr => {
+            return this.state.topLevel[name] ?? new E.Blank(E.exprData(`${name} not found`));
+        },
     };
-
-    removeTopLevel(name: string) {
-        this.setState(({ topLevel }) => {
-            const cloned = Object.assign({}, topLevel);
-            delete cloned[name];
-            return { topLevel: cloned };
-        });
-    }
-
-    setTopLevel(name: string, update: (expr: Expr) => Expr) {
-        this.setState(({ topLevel }) => ({
-            topLevel: { ...topLevel, [name]: update(topLevel[name]) },
-        }));
-    }
-
-    getTopLevel(name: string): Expr {
-        return this.state.topLevel[name] ?? new E.Blank(E.exprData(`${name} not found`));
-    }
 
     render() {
         return <Workspace.Provider value={this.state}>{this.props.children}</Workspace.Provider>;
@@ -52,7 +42,6 @@ export interface ClipboardValue {
 }
 
 export const Clipboard = React.createContext<Optional<ClipboardValue>>(null);
-
 export function ClipboardProvider({ children }: { children: ReactNode }) {
     const [clipboard, setClipboard] = useState<Expr[]>([]);
     const value = { clipboard, setClipboard };
