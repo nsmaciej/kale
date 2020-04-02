e654a50085e7573ced2577263d747da796bfb526
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-04-02 09:10:02 +0100

    Debug Overlay Improvements

--- web/src/components/debug_overlay.tsx
+++ web/src/components/debug_overlay.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 import { Area } from "expr_view/core";
 import { Offset, Rect, Size } from "geometry";
@@ -7,8 +7,11 @@ import { useUsesDarkTheme } from "contexts/theme";
 
 export default function SvgDebugOverlay({ area }: { area: Area }) {
     const darkTheme = useUsesDarkTheme();
-    const [onlyGaps, setOnlyGaps] = useState(false);
+    const [showAreas, setShowAreas] = useState(true);
+    const [showGaps, setShowGaps] = useState(false);
+    const [showIds, setShowIds] = useState(false);
     function text(rect: Rect, content: number | string) {
+        if (!showIds) return null;
         return (
             <text
                 x={rect.x + 1}
@@ -23,17 +26,19 @@ export default function SvgDebugOverlay({ area }: { area: Area }) {
 
     function renderArea(subArea: Area, origin: Offset): JSX.Element | null {
         const rect = subArea.rect.shift(origin);
+        const key = subArea.expr.id;
         if (subArea.kind === "gap") {
+            if (!showGaps) return null;
             return (
-                <>
+                <Fragment key={`gap-${key}`}>
                     <SvgRect rect={rect} fill="none" stroke="green" opacity={0.7} />
                     {text(rect, subArea.expr.id)}
-                </>
+                </Fragment>
             );
         }
         return (
-            <>
-                {onlyGaps || (
+            <Fragment key={`expr-${key}`}>
+                {showAreas && (
                     <SvgRect
                         rect={rect}
                         fill="none"
@@ -41,23 +46,43 @@ export default function SvgDebugOverlay({ area }: { area: Area }) {
                         opacity="0.7"
                     />
                 )}
-                {onlyGaps || text(rect, subArea.expr.id)}
+                {showAreas && text(rect, subArea.expr.id)}
                 {subArea.text != null && (
                     <DebugRect origin={rect.origin.offset(subArea.text?.offset ?? Offset.zero)} />
                 )}
                 {subArea.children.map((x) => renderArea(x, rect.origin))}
-            </>
+            </Fragment>
+        );
+    }
+
+    function renderButton(
+        rank: number,
+        type: string,
+        setState: (updater: (old: boolean) => boolean) => void,
+    ) {
+        return (
+            <text
+                fontSize={10}
+                y={10 + rank * 15}
+                x={area.rect.width + area.rect.x}
+                textAnchor="end"
+                key={type}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setState((x) => !x);
+                }}
+            >
+                Toggle {type}
+            </text>
         );
     }
 
     return (
         <>
-            <SvgRect
-                rect={new Rect(Offset.zero, new Size(5))}
-                fill="purple"
-                onClick={() => setOnlyGaps((x) => !x)}
-            />
             {renderArea(area, Offset.zero)}
+            {renderButton(0, "Gaps", setShowGaps)}
+            {renderButton(1, "IDs", setShowIds)}
+            {renderButton(2, "Areas", setShowAreas)}
         </>
     );
 }
